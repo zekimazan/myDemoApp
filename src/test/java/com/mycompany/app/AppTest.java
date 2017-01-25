c635a6c6632ec0e438e7384ba0fd33525c808b4a
zekimazan <zekimazan@gmail.com>
2017-01-25 13:56:17 +0300

    Bug fixed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -11,32 +11,6 @@ import java.util.Arrays;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
    public void testFound() {
       ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
       assertTrue(new App().search(array, 4));
